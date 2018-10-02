14cdad0e3605d1cc56b12d1e8e1d5a135176aebb
Winnie <user@es302n29>
2018-10-02 11:06:36 +0800

    variable

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,7 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		String name = "Winnie";
+		int age = 18;
+		float height = 155.8f;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		
 	}
 
 }
